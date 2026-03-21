760c5331454d90cf92ea18929e65a2ae89a57c63
ayushman chauhan <chauhanayushman505@gmail.com>
2026-03-21 23:13:40 +0530

    sql formatting fixed

--- init.lua
+++ init.lua
@@ -933,7 +933,9 @@ require('lazy').setup({
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         --c = true, cpp = true
-        local disable_filetypes = {}
+        local disable_filetypes = {
+          sql = true,
+        }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
